33be478d1305ae5226e891d801527b0135ec2cce
Valera Rozuvan <valera@rozuvan.net>
2022-05-06 22:23:17 +0300

    [scripts] add dockerize mongod

---
+++ scripts/dockerized_services/mongod/scripts/bash/migration.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+/data/scripts/bash/wait-for-db.sh
+
+echo "Creating users..."
+
+mongo --quiet \
+  admin \
+  /data/scripts/js/create-admin.js
+mongo --quiet \
+  -u admin \
+  -p "u45y38r3h2jt803urhj30892r3htr0" \
+  --authenticationDatabase admin \
+  crypton \
+  /data/scripts/js/create-user.js
+
+echo "[done]"
+
+exit 0
