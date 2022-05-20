d97e9b6f790b1e0ae0dafec591978b4b59f98f03
dasha <dariaausten@gmail.com>
2022-05-20 13:10:57 +0300

    crud rendr message list

--- pgm-client/src/components/Messenger/MessageList/MessageList.js
+++ pgm-client/src/components/Messenger/MessageList/MessageList.js
@@ -1,52 +1,82 @@
 import React from 'react';
 import { useState, useEffect, useRef } from 'react';
-// import { Message } from '../Message/Message'
+import { useSelector, useDispatch } from 'react-redux';
+import { Message } from '../Message/Message'
 import { Sender } from '../Sender/Sender';
 
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from '../../../theme';
 
-// import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 
-// import Typography from '@mui/material/Typography';
+import { ApiMessages } from '../../../api/apiMessages';
 
 function MessageList(props) {
-  // const [messages] = useState([]);
-  // const messagesEndRef = useRef(null);
+  const dispatch = useDispatch();
+  const messages = useSelector(state=>state.messages);
+  const messagesEndRef = useRef(null);
+  const [loading, setLoading] = useState(true)
 
-  // useEffect(() => {
-  //   scrollToBottom();
-  // }, [messages]);
+  useEffect(() => {
+    scrollToBottom();
+if (!loading) {
+			return;
+		}
 
-  // const scrollToBottom = () => {
-  //   return messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
-  // };
+
+    async function getAllMessages() {
+			try {
+				const api = new ApiMessages();
+				const result = await api.getMessages();
+				const messageList = await result.data;
+
+				dispatch({ type: 'SET_MESSAGES', data: messageList });
+
+				setLoading(false);
+
+			} catch (err) {
+				console.log('Failed to fetch messages.');
+			}
+			return true;
+		}
+
+  setLoading(true);
+  getAllMessages();
+  setLoading(false);
+
+
+  }, [messages, loading]);
+
+  const scrollToBottom = () => {
+    return messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
 
   return (
     <ThemeProvider theme={theme}>
 
       <Container>
         <Box>
-          {/* <Typography
-						variant="h6">messages from {props.activeKey.publicKey}
+          <Typography
+						variant="h6">messages from 
 					</Typography>
 				</Box>
 				<Box sx={{ width: "100%", height: "400px", overflowY: "scroll" }}>
 					<List >
 						{
-							props.activeKey.messages.map(({ sender, body, key }) => {
-								return (<Message sender={sender} body={body} key={key} />
+							messages.map(({ text, id, name}) => {
+								return (<Message text={text} id={id} key={id} name={name} />
 								)
 							})
 
 						}
 						<Box
-							style={{ float: "left", clear: "both", paddingTop: "1em", paddingBottom: "1em" }}
+							style={{backgroundColor:"#fcba03", float: "left", clear: "both", paddingTop: "1em", paddingBottom: "1em" }}
 							ref={messagesEndRef}>
 						</Box>
-					</List> */}
+					</List>
           <Sender/>
         </Box>
       </Container>
