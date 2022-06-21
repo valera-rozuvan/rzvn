0f6ff39d3283c2bde27cf57f2192b4ffc1403afb
DarinaSednevets <dariaausten@gmail.com>
2022-06-21 13:49:24 +0300

    add messaging between users

--- pgm-client/src/components/Messenger/Message/Message.js
+++ pgm-client/src/components/Messenger/Message/Message.js
@@ -2,21 +2,30 @@ import React from 'react';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Typography from '@mui/material/Typography';
+import { useSelector } from 'react-redux';
 
-function Message(props) {
+function Message({text, senderPublicKey}) {
+  const currentFriend = useSelector(state => state.currentFriend);
+	const currentUserPublicKey = useSelector(state => state.userKeys.userPublicKey);
+
+
   function color() {
-    return props.sender === 'me' ? '#ac9fbf' : '#f2b8ff';
+  return senderPublicKey === currentUserPublicKey ? '#ac9fbf' : '#f2b8ff';
   }
-
   function textAlign() {
-    return props.sender === 'me' ? 'left' : 'right';
+    return senderPublicKey  === currentUserPublicKey  ? 'right' : 'left';
+    console.log()
+  }
+  function getName() {
+    return senderPublicKey === currentUserPublicKey  ? 'me' : `${currentFriend.name}`;
+    console.log()
   }
 
   return (
     <ListItem sx={{ backgroundColor: color(), textAlign: textAlign() }}>
       <ListItemText>
-        <Typography >{props.name}</Typography>
-        <Typography >{props.text}</Typography>
+        <Typography >{getName()}</Typography>
+        <Typography >{text}</Typography>
       </ListItemText>
     </ListItem>
   );
