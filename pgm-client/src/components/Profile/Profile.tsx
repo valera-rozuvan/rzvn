e4be51a4a4386f508f1f7bb09e6adae019aae38e
DarinaSednevets <dariaausten@gmail.com>
2022-07-26 12:11:38 +0300

    add generate key, del support

--- pgm-client/src/components/Profile/Profile.tsx
+++ pgm-client/src/components/Profile/Profile.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-
-import Support from '../Support';
+import { Link } from 'react-router-dom';
 import FooterUser from '../FooterUser';
+
 import s from './profile.module.scss';
 
 function Profile() {
@@ -10,14 +10,16 @@ function Profile() {
       <section className={s.profile}>
         <p className={s.greating}>Welcome, user!</p>
         <p className={s.pkTitle}>public key</p>
-        <p className={s.pk}>0x894tr789utyt8ut8t2...</p>
+        <div className={s.keyBox}>
+          <p className={s.pk}>0x894tr789utyt8ut8t2...</p>
+          <Link to="/" className={s.generateBtn}>generate new key</Link>
+        </div>
         <p className={s.nickname}>nickname</p>
         <input placeholder="user name" />
         <div className={s.btnBox}>
           <button className={s.btnUpdate} type="submit">update</button>
           <button className={s.btnDelete} type="submit">delete all data</button>
         </div>
-        <Support />
       </section>
       <FooterUser />
 
